2af096992fb29d30165dbc9cf6ff0b47d02e04bd
qwltech <hughweilun@gmail.com>
2018-03-13 17:18:47 -0400

    Fix runtime error

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ resources.set("resource1", {});
 
 // collection of tasks assigned to resources
 var tasks = new Map();
-tasks.set(0, {"resource": resources("resource0")});
-tasks.set(1, {"resource": resources("resource1")});
+tasks.set(0, {"resource": resources.get("resource0")});
+tasks.set(1, {"resource": resources.get("resource1")});
