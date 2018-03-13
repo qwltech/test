ee638d58d908e0c5ca340ba63ef801b4ec91960a
qwltech <hughweilun@gmail.com>
2018-03-13 17:21:53 -0400

    Associate string instead of object

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 var resources = new Map();
 resources.set("resource0", {});
 resources.set("resource1", {});
+resources = 
 
 // collection of tasks assigned to resources
 var tasks = new Map();
-tasks.set(0, {"resource": resources.get("resource0")});
-tasks.set(1, {"resource": resources.get("resource1")});
+tasks.set(0, {"resource": "resource0"});
+tasks.set(1, {"resource": "resource1"});
